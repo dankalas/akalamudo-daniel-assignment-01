6ce92171603e121e3f59fec65d6a215045a974dd
dankalas <32338189+dankalas@users.noreply.github.com>
2023-08-30 21:43:34 +0000

    attempt-01

--- src/assignment.js
+++ src/assignment.js
@@ -16,11 +16,14 @@ const assignment = {};
  */
 function sumOfNumbersTo(destination) {
     let sum = 0;
+    for (let i = 1; i <= destination; i++){
+        sum += i;
+    }
     return sum
 }
 
 
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
+ assignment.sumOfNumbersTo = sumOfNumbersTo;
 
 /**
  * Challenge - 2
@@ -39,6 +42,14 @@ function countEvenNumbersWithin(destination) {
     let count = 0;
     let arrayOfEvenNumbers = [];
 
+    for(let i = 1; i <= destination; i++){
+        if (i % 2 === 0){
+            arrayOfEvenNumbers.push(i);
+            count++
+            sum += i;
+        }
+    }
+
     return {
         // property value shorthand
         // when the property name and the value name are the same
@@ -49,7 +60,7 @@ function countEvenNumbersWithin(destination) {
     };
 }
 
-// assignment.countEvenNumbersWithin = countEvenNumbersWithin;
+ assignment.countEvenNumbersWithin = countEvenNumbersWithin;
 
 /**
  * Challenge - 3
@@ -72,7 +83,7 @@ function celsiusToFahrenheit(arrayOfNumbers) {
     return result;
 }
 
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+ //assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 // ========================
 // DO NOT EDIT THIS BLOCK
